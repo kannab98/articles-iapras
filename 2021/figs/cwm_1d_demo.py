c9d8930890b15ed3d78fa72fcd65ae0e7b848249
kannab98 <ponur0kirill@yandex.ru>
2021-01-11 23:41:25 +0300

    add pics

--- 2021/figs/cwm_1d_demo.py
+++ 2021/figs/cwm_1d_demo.py
@@ -4,12 +4,18 @@ import numpy as np
 
 srf1 = kernel.simple_launch(cuda.cwm)
 srf0 = kernel.simple_launch(cuda.default)
+
+srf1[1] = np.rad2deg(np.arctan(srf1[1]))
+srf1[2] = np.rad2deg(np.arctan(srf1[2]))
+srf0[1] = np.rad2deg(np.arctan(srf0[1]))
+srf0[2] = np.rad2deg(np.arctan(srf0[2]))
+
 x = surface.gridx
 
 figs = []
 axs = []
-lbly = ["$\\zeta(x)$, м", "$\\sigma_x(x)$, м", "$\\sigma_y(x)$, м",
-         "$v_z(x)$, м", "$v_x(x)$, м", "$v_y(x)$, м"]
+lbly = ["$\\zeta(x)$, м", "$\\sigma_x(x)$, град", "$\\sigma_y(x)$, град",
+         "$v_z(x)$, м/с", "$v_x(x)$, м/с", "$v_y(x)$, м/с"]
 
 nms = ["h", "sigmax", "sigmay", "vz", "vx", "vy"]
 
@@ -17,8 +23,8 @@ for i in range(len(nms)):
     fig, ax = plt.subplots()
     figs.append(fig)
     axs.append(ax)
-    axs[-1].plot(x, srf1[i])
     axs[-1].plot(x, srf0[i])
+    axs[-1].plot(x, srf1[i])
     axs[-1].set_xlabel("$x$, м")
     axs[-1].set_ylabel(lbly[i])
 
