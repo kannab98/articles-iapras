80c1662038a168b0c6a594ba3a13090447c4eb35
kannab98 <ponur0kirill@yandex.ru>
2021-01-22 02:08:20 +0300

    add pics

--- 2021/figs/crosssec_real.py
+++ 2021/figs/crosssec_real.py
@@ -1,23 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib import cm
 
 df = pd.read_csv('Sig01.txt', sep='\s+', header=None)
 
-fig, ax = plt.subplots(ncols = 2, figsize=(8,4))
+fig, ax = plt.subplots(ncols = 1, figsize=(8,4))
 sigma = df.values.T
 
 theta = np.linspace(-18, 18, df.shape[1])
 x = np.arange(0, 5e3*df.shape[0], 5e3)
 
 X,Y = np.meshgrid(x, theta)
-mappable = ax[0].contourf(X,Y, sigma)
+mappable = ax.contourf(X,Y, sigma, levels=100, cmap=cm.plasma)
 
-ax[0].set_xlabel("$X,$ м")
-ax[0].set_ylabel("$\\theta,$ град")
+ax.set_xlabel("$X,$ м")
+ax.set_ylabel("$\\theta,$ град")
 bar = fig.colorbar(mappable=mappable)
 bar.set_label("$\\sigma_0$, dB")
 
-ax[1].plot(theta, sigma[:,0])
+# ax[1].plot(theta, sigma[:,0])
 
 fig.savefig('real_crosssec.png')
