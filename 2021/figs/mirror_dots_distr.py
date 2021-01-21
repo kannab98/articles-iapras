1e238f9e6d178ee760c7a181bdf637113606e86a
kannab98 <ponur0kirill@yandex.ru>
2021-01-22 01:59:19 +0300

    add pics

--- 2021/figs/mirror_dots_distr.py
+++ 2021/figs/mirror_dots_distr.py
@@ -7,15 +7,15 @@ from modeling import Experiment
 sat = Experiment.experiment()
 
 
-rc.surface.x = [-2500, 2500]
-rc.surface.y = [-2500, 2500]
+rc.surface.x = [-5, 5]
+rc.surface.y = [-5, 5]
+rc.wind.speed = 10
 rc.surface.gridSize = [256, 256]
 rc.surface.phiSize = 128
 rc.surface.kSize = 1024
 
 srf = kernel.launch(cuda.default)
 srf0 = kernel.convert_to_band(srf, 'Ku')
-# srf1 = kernel.convert_to_band(srf, 'Ka')
 
 
 
@@ -33,15 +33,55 @@ df = pd.DataFrame({"x": x, "y": y,
                     "velocity x": srf0[4].flatten(),
                     "velocity y": srf0[5].flatten(), })
 
+z = df['elevation'].values.reshape(rc.surface.gridSize)
 
 
 surface.coordinates = [x, y, df['elevation'] ]
 surface.normal = [df['slopes x'], df['slopes y'], np.ones(x.size) ]
 cov = np.cov(df['slopes x'], df['slopes y'])
 
+theta0 = sat.localIncidence(xi=0)
+indKu = sat.sort(theta0)
 
-z = df['elevation'].values.reshape(rc.surface.gridSize)
+
+sat = Experiment.experiment()
+srf1 = kernel.convert_to_band(srf, 'Ka')
+print(srf1.all() == srf0.all())
+df = pd.DataFrame({"x": x, "y": y, 
+                    "elevation": srf1[0].flatten(),
+                    "slopes x":  srf1[1].flatten(),
+                    "slopes y":  srf1[2].flatten(),
+                    "velocity z": srf1[3].flatten(),
+                    "velocity x": srf1[4].flatten(),
+                    "velocity y": srf1[5].flatten(), })
+
+surface.coordinates = [x, y, df['elevation'] ]
+surface.normal = [df['slopes x'], df['slopes y'], np.ones(x.size) ]
+cov = np.cov(df['slopes x'], df['slopes y'])
 
 theta0 = sat.localIncidence(xi=0)
+indKa = sat.sort(theta0)
+# theta0 = theta0.reshape(rc.surface.gridSize)
+
+# fig, ax  = plt.subplots(figsize=(5,4))
+# X, Y = surface.meshgrid
+# mappable = ax.contourf(X,Y,np.rad2deg(theta0))
+# fig.colorbar(mappable=mappable)
+# fig.savefig('mirrordots.png')
+
+
+fig, ax  = plt.subplots(figsize=(5,4))
+X, Y = surface.meshgrid
+mappable = ax.contourf(X,Y, z, levels=100)
+ax.scatter(X.flatten()[indKu],Y.flatten()[indKu], marker='.', label="Ku, n=%s " % indKu[0].size)
+ax.scatter(X.flatten()[indKa],Y.flatten()[indKa], marker='.', label="Ka, n=%s" % indKa[0].size)
+ax.set_xlabel("$X$, м")
+ax.set_ylabel("$Y$, м")
+
+bar = fig.colorbar(mappable=mappable)
+bar.set_label("$\\zeta$, м")
+
+fig.legend(loc="upper center")
+fig.savefig('mirrordots.png')
 
 
