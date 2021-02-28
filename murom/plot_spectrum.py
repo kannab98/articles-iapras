b0ef2b29a9d5596b7154fd0f71515200bf19fe94
kannab98 <ponur0kirill@yandex.ru>
2021-03-01 00:50:46 +0300

    pics

--- murom/plot_spectrum.py
+++ murom/plot_spectrum.py
@@ -8,11 +8,12 @@ from seawave import rc
 
 plt.style.use(['science', 'ieee'])
 
+rc.wind.direction = 0
 
 U  = [5, 10, 15]
 X  = [3000, 8000, 15000, 20170]
-
-
+Km = [0.5, 1, 2, 5]
+phi = np.linspace(-np.pi, np.pi, 257)
 
 
 
@@ -42,4 +43,14 @@ with PdfPages('spectrum.pdf') as pdf:
     ax.set_xlabel("$\\kappa, \\text{рад} \\cdot \\text{ м}^{-1}$")
     ax.set_ylabel("$S(\\kappa), \\text{м}^3 \\cdot \\text{ рад}^{-1}$")
     ax.legend()
+    pdf.savefig()
+
+    fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+    for km in Km:
+        S = spectrum.azimuthal_distribution(km, phi).T
+        ax.plot(phi, S, label="$\\kappa/\\kappa_m = %.1f$" % km)
+
+    ax.set_xlabel("$\\Phi(\\kappa, \\phi)$")
+    ax.legend()
+
     pdf.savefig()
